dff797e400fbad00bd5e8f727f49fe303741dcc0
moedeera <82620264+moedeera@users.noreply.github.com>
2022-07-28 15:50:11 -0600

    adding html

--- index.php
+++ index.php
@@ -15,3 +15,19 @@ echo "Heroku site fo Moe"
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My PHP Website</title>
+</head>
+<body>
+  <!-- You can output PHP including variables, etc -->
+  <h1>Hello <?php echo "name is $name"; ?></h1>
+  <!-- You may only drop the semi-colon after a statement when the statement is followed immediately by a closing PHP tag ?>. -->
+  <h1>Hello heroku from PHP <?= $name ?></h1>
+</body>
+</html>
